6ab587b20130a447b548a8f6dcab4ec30a2070bf
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-09-12 16:06:29 +0000

    add aodvv2 code without external Makefile edits

---
+++ sys/net/routing/aodvv2/constants.h
@@ -0,0 +1,89 @@
+/*
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     aodvv2
+ * @{
+ *
+ * @file        constants.h
+ * @brief       constants for the aodvv2 routing protocol
+ *
+ * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
+ */
+
+#include "common/netaddr.h"
+#include "rfc5444/rfc5444_print.h"
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define AODVV2_MAX_HOPCOUNT 20
+#define AODVV2_MAX_ROUTING_ENTRIES 255
+#define AODVV2_DEFAULT_METRIC_TYPE 3
+#define AODVV2_ACTIVE_INTERVAL 5         // seconds
+#define AODVV2_MAX_IDLETIME 250          // seconds
+#define AODVV2_MAX_SEQNUM_LIFETIME 300   // seconds
+#define AODVV2_RIOT_PREFIXLEN 128        // TODO: find better name
+#define AODVV2_MAX_UNREACHABLE_NODES 15  // TODO: choose value (wisely)
+
+/* RFC5498 */
+#define MANET_PORT  269
+
+/* my multicast address */
+struct netaddr na_mcast;
+
+/* AODVv2 message types */
+enum rfc5444_msg_type
+{
+    RFC5444_MSGTYPE_RREQ = 10,
+    RFC5444_MSGTYPE_RREP = 11,
+    RFC5444_MSGTYPE_RERR = 12,
+};
+
+/* AODVv2 TLV types */
+enum rfc5444_tlv_type
+{
+    RFC5444_MSGTLV_ORIGSEQNUM,
+    RFC5444_MSGTLV_TARGSEQNUM,
+    RFC5444_MSGTLV_UNREACHABLE_NODE_SEQNUM,
+    RFC5444_MSGTLV_METRIC,
+};
+
+/* TLV type array indices */
+enum tlv_index
+{
+    TLV_ORIGSEQNUM,
+    TLV_TARGSEQNUM,
+    TLV_UNREACHABLE_NODE_SEQNUM,
+    TLV_METRIC,
+};
+
+struct node_data
+{
+    struct netaddr addr;
+    uint8_t metric;
+    uint16_t seqnum;
+};
+
+/* all data contained in a RREQ or RREP */
+struct aodvv2_packet_data
+{
+    uint8_t hoplimit;
+    struct netaddr sender;
+    uint8_t metricType;
+    struct node_data origNode;
+    struct node_data targNode;
+    timex_t timestamp;
+};
+
+struct unreachable_node
+{
+    struct netaddr addr;
+    uint16_t seqnum;
+};
+
+#endif /* CONSTANTS_H_ */
---
+++ sys/net/routing/aodvv2/writer.h
@@ -0,0 +1,83 @@
+/*
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     aodvv2
+ * @{
+ *
+ * @file        writer.c
+ * @brief       writer to create RFC5444 aodvv2 messages
+ *
+ * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
+ */
+
+#ifndef WRITER_H_
+#define WRITER_H_
+
+#include "common/common_types.h"
+#include "rfc5444/rfc5444_writer.h"
+#include "reader.h"
+
+#include "common/common_types.h"
+#include "common/netaddr.h"
+#include "rfc5444/rfc5444_writer.h"
+#include "rfc5444/rfc5444_iana.h"
+#include "mutex.h"
+
+#include "constants.h"
+#include "seqnum.h"
+
+struct writer_target
+{
+    struct rfc5444_writer_target interface;
+    struct netaddr target_addr;
+    struct aodvv2_packet_data packet_data;
+    int type;
+};
+
+typedef void (*write_packet_func_ptr)(
+    struct rfc5444_writer *wr, struct rfc5444_writer_target *iface, void *buffer, size_t length);
+
+/**
+ * Initialize RFC5444 writer
+ * @param ptr pointer to "send_packet" callback
+ */
+void writer_init(write_packet_func_ptr ptr);
+
+/**
+ * Clean up after the RFC5444 writer
+ */
+void writer_cleanup(void);
+
+/**
+ * Send a RREQ. DO NOT use this function to dispatch packets from anything else
+ * than the sender_thread. To send RREQs, use aodv_send_rreq().
+ * @param packet_data parameters of the RREQ
+ * @param next_hop Address the RREP is sent to
+ */
+void writer_send_rreq(struct aodvv2_packet_data *packet_data, struct netaddr *next_hop);
+
+/**
+ * Send a RREP. DO NOT use this function to dispatch packets from anything else
+ * than the sender_thread. To send RREPs, use aodv_send_rrep().
+ * @param packet_data parameters of the RREP
+ * @param next_hop Address the RREP is sent to
+ */
+void writer_send_rrep(struct aodvv2_packet_data *packet_data, struct netaddr *next_hop);
+
+/**
+ * Send a RERR. DO NOT use this function to dispatch packets from anything else
+ * than the sender_thread. To send RERRs, use aodv_send_rerr().
+ * @param unreachable_nodes[] array containing all newly unreachable nodes. each
+ *                            in a struct unreachable_node
+ * @param len                 length of unreachable_nodes[]
+ * @param hoplimit            the message's hop limit
+ * @param next_hop            Address the RREP is sent to
+ */
+void writer_send_rerr(struct unreachable_node unreachable_nodes[], int len, int hoplimit, struct netaddr *next_hop);
+
+#endif /* WRITER_H_ */
