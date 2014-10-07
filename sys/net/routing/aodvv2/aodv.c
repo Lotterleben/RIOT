b7e2998f2a1f99d548b0aee0786e9fe0e588e4ee
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-10-07 05:52:50 -0700

    + TODO about RCV_MSG_Q size

--- sys/net/routing/aodvv2/aodv.c
+++ sys/net/routing/aodvv2/aodv.c
@@ -22,7 +22,7 @@
 #include "debug.h"
 
 #define UDP_BUFFER_SIZE     (128) /* with respect to IEEE 802.15.4's MTU */
-#define RCV_MSG_Q_SIZE      (64)
+#define RCV_MSG_Q_SIZE      (64)  /* TODO: check if smaller values work, too*/
 
 static void _init_addresses(void);
 static void _init_sock_snd(void);
