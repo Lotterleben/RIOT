76481cb0687675f4d08f62a5f64351aea86fb5e3
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-10-07 05:30:22 -0700

    + documentation for aodvv2.h

--- sys/net/include/aodvv2/aodvv2.h
+++ sys/net/include/aodvv2/aodvv2.h
@@ -1,10 +1,30 @@
+/**
+ * @ingroup     aodvv2
+ * @{
+ *
+ * @file        aodvv2.h
+ * @brief       aodvv2 routing protocol
+ *
+ * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
+ */
+
 #include "common/netaddr.h"
 #include "rfc5444/rfc5444_print.h"
 
 #ifndef AODVV2_H_
 #define AODVV2_H_
 
+/**
+ * @brief   Initialize the AODVv2 routing protocol.
+ */
 void aodv_init(void);
+
+/**
+ * @brief   Set the metric type. If metric_type does not match any known metric
+ *          types, no changes will be made.
+ *
+ * @param[in] metric_type       type of new metric
+ */
 void aodv_set_metric_type(int metric_type);
 
 #endif /* AODVV2_H_ */
