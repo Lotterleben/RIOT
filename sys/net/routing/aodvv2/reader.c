c8713d4de75da511a3bad6c9ec3a99cfe48d7571
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-10-08 11:12:25 -0700

    Improve license

--- sys/net/routing/aodvv2/reader.c
+++ sys/net/routing/aodvv2/reader.c
@@ -1,8 +1,10 @@
 /*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 /**
