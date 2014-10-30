9cfb848c62df7b211954c870de0c8d65bf178bf7
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-10-30 16:20:10 -0700

    types.h and constants.h reordering
    move *_type enums and structs node_data, aodvv2_packet_data and unreachable_node from constants.h to types.h

--- sys/net/include/aodvv2/types.h
+++ sys/net/include/aodvv2/types.h
@@ -20,6 +20,8 @@
 #ifndef AODVV2_TYPES_H
 #define AODVV2_TYPES_H
 
+#include "common/netaddr.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -34,6 +36,64 @@ typedef enum {
 typedef uint16_t aodvv2_seqnum_t;
 
 #define AODVV2_DEFAULT_METRIC_TYPE HOP_COUNT
+
+/**
+ * @brief   AODVv2 message types
+ */
+enum rfc5444_msg_type
+{
+    RFC5444_MSGTYPE_RREQ = 10,
+    RFC5444_MSGTYPE_RREP = 11,
+    RFC5444_MSGTYPE_RERR = 12,
+};
+
+/**
+ * @brief   AODVv2 TLV types
+ */
+enum rfc5444_tlv_type
+{
+    RFC5444_MSGTLV_ORIGSEQNUM,
+    RFC5444_MSGTLV_TARGSEQNUM,
+    RFC5444_MSGTLV_UNREACHABLE_NODE_SEQNUM,
+    RFC5444_MSGTLV_METRIC,
+};
+
+/**
+ * @brief   Data about an OrigNode or TargNode, typically embedded in an
+ *          aodvv2_packet_data struct.
+ */
+struct node_data
+{
+    struct netaddr addr;                /**< IP address of the node */
+    uint8_t metric;                     /**< Metric value */
+    aodvv2_seqnum_t seqnum;             /**< Sequence Number */
+};
+
+/**
+ * @brief   all data contained in a RREQ or RREP.
+ */
+struct aodvv2_packet_data
+{
+    uint8_t hoplimit;                   /**< Hop limit */
+    struct netaddr sender;              /**< IP address of the neighboring router
+                                         *   which sent the RREQ/RREP*/
+    aodvv2_metric_t metricType;         /**< Metric type */
+    struct node_data origNode;          /**< Data about the originating node */
+    struct node_data targNode;          /**< Data about the originating node */
+    timex_t timestamp;                  /**< point at which the packet was (roughly)
+                                         *   received. Note that this timestamp
+                                         *   will be set after the packet has been
+                                         *   successfully parsed. */
+};
+
+/**
+ * @brief   Data about an unreachable node to be embedded in a RERR.
+ */
+struct unreachable_node
+{
+    struct netaddr addr;                        /**< IP address */
+    aodvv2_seqnum_t seqnum;                     /**< Sequence Number */
+};
 
 #ifdef  __cplusplus
 }
--- sys/net/routing/aodvv2/constants.h
+++ sys/net/routing/aodvv2/constants.h
@@ -41,27 +41,6 @@ enum aodvv2_constants {
 };
 
 /**
- * @brief   AODVv2 message types
- */
-enum rfc5444_msg_type
-{
-    RFC5444_MSGTYPE_RREQ = 10,
-    RFC5444_MSGTYPE_RREP = 11,
-    RFC5444_MSGTYPE_RERR = 12,
-};
-
-/**
- * @brief   AODVv2 TLV types
- */
-enum rfc5444_tlv_type
-{
-    RFC5444_MSGTLV_ORIGSEQNUM,
-    RFC5444_MSGTLV_TARGSEQNUM,
-    RFC5444_MSGTLV_UNREACHABLE_NODE_SEQNUM,
-    RFC5444_MSGTLV_METRIC,
-};
-
-/**
  * @brief   TLV type array indices
  */
 enum tlv_index
@@ -74,43 +53,6 @@ enum tlv_index
 
 /* my multicast address */
 struct netaddr na_mcast;
-
-/**
- * @brief   Data about an OrigNode or TargNode, typically embedded in an
- *          aodvv2_packet_data struct.
- */
-struct node_data
-{
-    struct netaddr addr;                /**< IP address of the node */
-    uint8_t metric;                     /**< Metric value */
-    aodvv2_seqnum_t seqnum;             /**< Sequence Number */
-};
-
-/**
- * @brief   all data contained in a RREQ or RREP.
- */
-struct aodvv2_packet_data
-{
-    uint8_t hoplimit;                   /**< Hop limit */
-    struct netaddr sender;              /**< IP address of the neighboring router
-                                         *   which sent the RREQ/RREP*/
-    aodvv2_metric_t metricType;         /**< Metric type */
-    struct node_data origNode;          /**< Data about the originating node */
-    struct node_data targNode;          /**< Data about the originating node */
-    timex_t timestamp;                  /**< point at which the packet was (roughly)
-                                         *   received. Note that this timestamp
-                                         *   will be set after the packet has been
-                                         *   successfully parsed. */
-};
-
-/**
- * @brief   Data about an unreachable node to be embedded in a RERR.
- */
-struct unreachable_node
-{
-    struct netaddr addr;                        /**< IP address */
-    aodvv2_seqnum_t seqnum;                     /**< Sequence Number */
-};
 
 #ifdef  __cplusplus
 }
